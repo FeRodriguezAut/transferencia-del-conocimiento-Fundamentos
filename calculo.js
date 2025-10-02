42f4f5790377cdb4ebc31636b2bcc6bf4975f119
Fer Rodriguez <32382866+FeRodriguezAut@users.noreply.github.com>
2025-10-01 19:35:16 -0500

    new file:   calculo.js 	modified:   index.html

---
+++ calculo.js
@@ -0,0 +1,4 @@
+let producto = parseFloat(prompt("Ingrese Precio del producto:"));
+let descuento = producto * 0.10;
+let total = producto - descuento;
+alert("El precio con descuento es: " + total);
